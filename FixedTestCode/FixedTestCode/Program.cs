fc7dbba7e66c430cfdd6a4805c163680d12f6761
benLH20 <116368358+benLH20@users.noreply.github.com>
2024-10-07 00:15:30 +0300

    Add files via upload

---
+++ FixedTestCode/FixedTestCode/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace FixedTestCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        //: שאלה מספר 1
+        public static bool IsNice(int[] arr)
+        {
+            int currSum = 0, cnt = 0;
+            if (arr[0] != 0)
+            {
+                return false;
+            }
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] == -999)
+                {
+                    if (i == arr.Length - 1)
+                    {
+                        return false;
+                    }
+                    if (arr[i + 1] != currSum)
+                    {
+                        return false;
+                    }
+                    currSum = 0;
+                    cnt = 0;
+                }
+                else if (i < arr.Length - 1)
+                {
+                    currSum += arr[i];
+                    cnt++;
+                }
+                else if (arr[i] != currSum || cnt != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+    }
+}
